d27bbcb1801cc4ff3ab9914df06aeff2602315f2
rlbarter <rlbarter@gmail.com>
2017-06-29 10:34:19 -0700

    Edited deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -36,5 +36,5 @@ git commit -m "$MESSAGE"
 git push origin "$SOURCE"
 
 msg "Pushing the updated \`public\` folder to the \`master\` branch"
-git subtree push -f --prefix=public \
-	git@github.com:$USERNAME/$USERNAME.github.io.git master
+git subtree push --prefix=public git@github.com:$USERNAME/$USERNAME.github.io.git master
+msg "Done"
